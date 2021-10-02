0bcc4acd11a9a8eb648251dea7363416aa50d573
heon2100 <91506032+heon2100@users.noreply.github.com>
2021-10-02 18:34:15 +0900

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,8 +1,16 @@
 window.onload = function() {
     var burgerBtn = document.getElementById("burgerBtn");
     var mobile = document.getElementById("mobile");
-
+    var nav = document.getElementById("nav");
     burgerBtn.addEventListener(
+        "click",
+        function() {
+            mobile.classList.toggle("navigation");
+        },
+        false
+    );
+
+    nav.addEventListener(
         "click",
         function() {
             mobile.classList.toggle("navigation");
